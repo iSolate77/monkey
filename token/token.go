533d5b3aba73bd27e9d5034fcbe004c4cc3d4d86
Mohamed Faris <mohd.faris07@gmail.com>
2023-04-17 22:00:58 +0300

    We have more operators, keywords and we have equals or not equals

--- token/token.go
+++ token/token.go
@@ -18,6 +18,16 @@ const (
   // Operators
   ASSIGN = "="
   PLUS = "+"
+  MINUS = "-"
+  BANG = "!"
+  ASTERISK = "*"
+  SLASH = "/"
+
+  LT = "<"
+  GT = ">"
+
+  EQ = "=="
+  NOT_EQ = "!="
 
   // Delimiters
   COMMA = ","
@@ -31,11 +41,21 @@ const (
   // Keywords
   FUNCTION = "FUNCTION"
   LET = "LET"
+  IF = "IF"
+  ELSE = "ELSE"
+  TRUE = "TRUE"
+  FALSE = "FALSE"
+  RETURN = "RETURN"
 )
 
 var keywords = map[string]TokenType{
   "fn": FUNCTION,
   "let": LET,
+  "if": IF,
+  "else": ELSE,
+  "true": TRUE,
+  "false": FALSE,
+  "return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
