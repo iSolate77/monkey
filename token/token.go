be0efb8dbe59199431f4b857c3f42659b2df05fe
Mohamed Faris <mohd.faris07@gmail.com>
2024-04-02 15:49:37 +0300

    chapter 2

--- token/token.go
+++ token/token.go
@@ -15,52 +15,52 @@ const (
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"   // 1343456
 
-  // Operators
-  ASSIGN = "="
-  PLUS = "+"
-  MINUS = "-"
-  BANG = "!"
-  ASTERISK = "*"
-  SLASH = "/"
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
 
-  LT = "<"
-  GT = ">"
+	LT = "<"
+	GT = ">"
 
-  EQ = "=="
-  NOT_EQ = "!="
+	EQ     = "=="
+	NOT_EQ = "!="
 
-  // Delimiters
-  COMMA = ","
-  SEMICOLON = ";"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
 
-  LPAREN = "("
-  RPAREN = ")"
-  LBRACE = "{"
-  RBRACE = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-  // Keywords
-  FUNCTION = "FUNCTION"
-  LET = "LET"
-  IF = "IF"
-  ELSE = "ELSE"
-  TRUE = "TRUE"
-  FALSE = "FALSE"
-  RETURN = "RETURN"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-  "fn": FUNCTION,
-  "let": LET,
-  "if": IF,
-  "else": ELSE,
-  "true": TRUE,
-  "false": FALSE,
-  "return": RETURN,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
-  if tok, ok := keywords[ident]; ok {
-    return tok
-  }
-  return IDENT
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
